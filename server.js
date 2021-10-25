4f7bc5186f7e7ccdf89a5d7bd3cbb3e2a7bde507
yousoumar <oumaiyr@gmail.com>
2021-10-25 20:59:00 +0200

    hooks up the server with react

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const { userRouter } = require("./routes/userRouters");
 const { authUser } = require("./middlewares/authentication");
 
 const app = express();
+app.use(express.static("./views/build"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", authUser, (req, res) => {
-  res.send("Hello there");
-});
-
 app.use(userRouter);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/build/index.html"));
+});
 
 const DBURL = process.env.DBURL;
 const PORT = process.env.PORT;
