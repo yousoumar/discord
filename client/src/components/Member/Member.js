f4c0ed713f2d53a2d63e48ad50519e30de046b2b
yousoumar <oumaiyr@gmail.com>
2021-12-18 09:57:20 +0100

    changes avatar

--- client/src/components/Member/Member.js
+++ client/src/components/Member/Member.js
@@ -3,14 +3,7 @@ import "./Member.scss";
 export default function Member({ member, hideName, online }) {
   return (
     <div className={online ? "member online" : "member"}>
-      <img
-        src={
-          member.picture
-            ? member.picture
-            : "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-        }
-        alt=""
-      />
+      <img src={member.picture ? member.picture : "/avatar.svg"} alt="" />
       {!hideName && (
         <div className="name">{member.name ? member.name : "A secret one"}</div>
       )}
